e09a4681b148107fd4abebe29e5b4d1f78c76e12
Mateus Gomes <mateus.gs1999@gmail.com>
2022-02-23 23:41:05 -0300

    configuração do firebase

---
+++ src/config/firebaseconfig.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+import 'firebase/storage';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCcpSr4boHa9pvsbVQGeFxmft3e3ZhyxXs",
+  authDomain: "task-6079a.firebaseapp.com",
+  projectId: "task-6079a",
+  storageBucket: "task-6079a.appspot.com",
+  messagingSenderId: "309941176187",
+  appId: "1:309941176187:web:3da48c04bad6a3316e5fde"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const database = firebase.firestore();
+export default database
