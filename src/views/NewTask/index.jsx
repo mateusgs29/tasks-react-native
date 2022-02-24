35ae8051d4757a5a84e2092d74286ba396b8e5b4
Mateus Gomes <mateus.gs1999@gmail.com>
2022-02-23 23:41:40 -0300

    pagina de nova task

---
+++ src/views/NewTask/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+
+export default function NewTask() {
+  return (
+    <View>
+      <Text>Page NewTask</Text>
+    </View>
+  )
+}
