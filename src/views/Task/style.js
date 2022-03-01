ab472c736fe3ee01be2a4d4277991eda6642df65
Mateus Gomes <mateus.gs1999@gmail.com>
2022-03-01 12:06:57 -0300

    finalização do estilo da lista de tasks

--- src/views/Task/style.js
+++ src/views/Task/style.js
@@ -6,6 +6,27 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     paddingTop: 20
   },
+  Tasks: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 5
+  },
+  deleteTask: {
+    justifyContent: "center",
+    paddingLeft: 15
+  },
+  DescriptionTask: {
+    width: "75%",
+    alignContent: "flex-start",
+    backgroundColor: "#f5f5f5",
+    padding: 12,
+    paddingHorizontal: 20,
+    borderRadius: 50,
+    marginBottom: 5,
+    marginRight: 15,
+    color: "#282b2db5"
+  },
   buttonNewTask: {
     width: 60,
     height: 60,
@@ -19,7 +40,7 @@ const styles = StyleSheet.create({
   },  
   iconButton: {
     color: "#fff",
-    fontSize: 20,
+    fontSize: 25,
     fontWeight: "bold"
   }
 })
