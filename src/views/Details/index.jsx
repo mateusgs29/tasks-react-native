cbbb931b352dccc3d21049d194bda2eb9ff31a85
Mateus Gomes <mateus.gs1999@gmail.com>
2022-03-01 12:08:24 -0300

    pagina de descricao da task, com edicao

--- src/views/Details/index.jsx
+++ src/views/Details/index.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { Text, TextInput, TouchableOpacity, View } from 'react-native'
+
+import database from '../../config/firebaseconfig'
+import styles from './style'
 
 
-export default function Details() {
+export default function Details({ navigation, route }) {
+  const [descriptionEdit, setDescriptionEdit] = useState(route.params.description)
+  const idTask = route.params.id
+
+  function editTask() {
+    database.collection("Tasks").doc(idTask).update({ description: descriptionEdit })
+    navigation.navigate("Task")
+  }
+
   return (
-    <View>
-      <Text>Page Details</Text>
+    <View style={styles.container}>
+      <Text style={styles.label}>Description</Text>
+      <TextInput 
+        style={styles.input}
+        placeholder="Ex: estudar javascript"
+        onChangeText={setDescriptionEdit}
+        value={descriptionEdit}
+      />
+      <TouchableOpacity
+        style={styles.buttonNewTask}
+        onPress={() => editTask()}
+      >
+        <Text style={styles.iconButton}>Edit</Text>
+      </TouchableOpacity>
     </View>
   )
 }
