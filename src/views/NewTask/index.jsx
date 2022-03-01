2a5eca6f3d04d2c56a177424c0b39abdc2d44c06
Mateus Gomes <mateus.gs1999@gmail.com>
2022-03-01 12:08:08 -0300

    pagina de criar uma nova task

--- src/views/NewTask/index.jsx
+++ src/views/NewTask/index.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { Text, TextInput, TouchableOpacity, View } from 'react-native'
+
+import database from '../../config/firebaseconfig'
+import styles from './style'
 
 
-export default function NewTask() {
+export default function NewTask({ navigation }) {
+  const [description, setDescription] = useState(null)
+
+  function addTask() {
+    database.collection("Tasks").add({
+      description: description,
+      status: false
+    })
+    navigation.navigate("Task")
+  }
+
   return (
-    <View>
-      <Text>Page NewTask</Text>
+    <View style={styles.container}>
+      <Text style={styles.label}>Description</Text>
+      <TextInput 
+        style={styles.input}
+        placeholder="Ex: estudar javascript"
+        onChangeText={setDescription}
+        value={description}
+      />
+      <TouchableOpacity
+        style={styles.buttonNewTask}
+        onPress={() => addTask()}
+      >
+        <Text style={styles.iconButton}>Save</Text>
+      </TouchableOpacity>
     </View>
   )
 }
